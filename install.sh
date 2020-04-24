68bae6f33170d709347325ea9f91e18776943ef1
Charles Machalow <csm10495@gmail.com>
2020-04-24 10:50:34 -0700

    add install

---
+++ install.sh
@@ -0,0 +1,4 @@
+pushd ~
+curl https://raw.githubusercontent.com/csm10495/dotfiles/master/home/.bash_profile > ~/.bash_profile
+curl https://raw.githubusercontent.com/csm10495/dotfiles/master/home/.bashrc > ~/.bashrc
+popd
