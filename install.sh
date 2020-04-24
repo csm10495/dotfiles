7604b2b5161e3e9805873d007e767f47bda54cbe
Charles Machalow <csm10495@gmail.com>
2020-04-24 11:03:05 -0700

    docs

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd ~
-curl https://raw.githubusercontent.com/csm10495/dotfiles/master/home/.bash_profile > ~/.bash_profile
-curl https://raw.githubusercontent.com/csm10495/dotfiles/master/home/.bashrc > ~/.bashrc
-popd
+curl -s https://raw.githubusercontent.com/csm10495/dotfiles/master/home/.bash_profile > ~/.bash_profile
+curl -s https://raw.githubusercontent.com/csm10495/dotfiles/master/home/.bashrc > ~/.bashrc
+
+echo Done!
