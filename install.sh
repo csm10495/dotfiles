008823d46365ab20529404555531b2f56a106a33
Charles Machalow <csm10495@gmail.com>
2020-04-24 10:53:28 -0700

    add shebang

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 pushd ~
 curl https://raw.githubusercontent.com/csm10495/dotfiles/master/home/.bash_profile > ~/.bash_profile
 curl https://raw.githubusercontent.com/csm10495/dotfiles/master/home/.bashrc > ~/.bashrc
