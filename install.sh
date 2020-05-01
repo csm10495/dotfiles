5ed9a29477a364c1fbd64ccb71422dfa38c53b0a
Charles Machalow <csm10495@gmail.com>
2020-04-30 20:50:28 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,10 +22,10 @@ if [[ "$PYTHON" != "" ]]; then
     COMMIT_HASH=`curl -s https://api.github.com/repos/csm10495/dotfiles/branches/master | $PYTHON -c "import sys, json; print(json.loads(sys.stdin.read())['commit']['sha'].upper())" 2>/dev/null`
     if [[ "$COMMIT_HASH" != "" ]]; then
         printf $COMMIT_HASH | $PYTHON -c "import os, sys; text = open(os.path.expanduser('~/.bashrc'), 'r').read();open(os.path.expanduser('~/.bashrc'), 'w').write(text.replace('REPLACE_WITH_REPO_HASH', sys.stdin.read().strip()))"
-    fi
     
-    VERSION=`curl -I -s "https://api.github.com/repos/csm10495/dotfiles/commits?per_page=1&sha=$COMMIT_HASH" | grep "&page=" | $PYTHON -c "import re,sys; print(re.findall(r'page=(\d+?)\>\; rel=\"last\"', sys.stdin.read())[0])" 2> /dev/null`
-    printf $VERSION | $PYTHON -c "import os, sys; text = open(os.path.expanduser('~/.bashrc'), 'r').read();open(os.path.expanduser('~/.bashrc'), 'w').write(text.replace('REPLACE_WITH_VERSION', sys.stdin.read().strip()))"
+        VERSION=`curl -I -s "https://api.github.com/repos/csm10495/dotfiles/commits?per_page=1&sha=$COMMIT_HASH" | grep "&page=" | $PYTHON -c "import re,sys; print(re.findall(r'page=(\d+?)\>\; rel=\"last\"', sys.stdin.read())[0])" 2> /dev/null`
+        printf $VERSION | $PYTHON -c "import os, sys; text = open(os.path.expanduser('~/.bashrc'), 'r').read();open(os.path.expanduser('~/.bashrc'), 'w').write(text.replace('REPLACE_WITH_VERSION', sys.stdin.read().strip()))"
+    fi
 fi
 
 echo Done!
