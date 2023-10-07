98f9402a75ae545e0632f7cee4506e05af17a3fb
Charles Machalow <csm10495@gmail.com>
2023-10-06 20:11:55 -0700

    Cleanup install logic

--- install.sh
+++ install.sh
@@ -1,37 +1,64 @@
 #!/bin/bash
 
-if [[ $(which curl 2>/dev/null) == "" ]]; then
+if ! command -v curl &>/dev/null; then
     echo "curl is required to run this script. Please install it and re-run!"
     exit 1
 fi
 
-PYTHON=""
-if [[ $(which python3 2>/dev/null) != "" ]]; then
-    PYTHON="python3"
-elif [[ $(which python 2>/dev/null) != "" ]]; then
-    PYTHON="python"
-elif [[ $(which python2 2>/dev/null) != "" ]]; then
-    PYTHON="python2"
+# Without jq , we can't parse json, so just use master.
+if command -v jq &>/dev/null; then
+    HAS_JQ=true
+else
+    HAS_JQ=false
 fi
 
-CURL_LOW_TIMEOUT="curl --connect-timeout 1 --max-time 1 "
-COMMIT_HASH=$($CURL_LOW_TIMEOUT -s https://api.github.com/repos/csm10495/dotfiles/branches/master | $PYTHON -c "import sys, json; print(json.loads(sys.stdin.read())['commit']['sha'].upper())" 2>/dev/null)
-if [[ "$COMMIT_HASH" != "" && "$PYTHON" != "" ]]; then
-
-    $CURL_LOW_TIMEOUT -s https://raw.githubusercontent.com/csm10495/dotfiles/$COMMIT_HASH/home/.bash_profile > /tmp/.bash_profile
-    $CURL_LOW_TIMEOUT -s https://raw.githubusercontent.com/csm10495/dotfiles/$COMMIT_HASH/home/.bashrc > /tmp/.bashrc
-
-    VERSION=$($CURL_LOW_TIMEOUT -I -s "https://api.github.com/repos/csm10495/dotfiles/commits?per_page=1&sha=$COMMIT_HASH" | grep "&page=" | $PYTHON -c "import re,sys; print(re.findall(r'page=(\d+?)\>\; rel=\"last\"', sys.stdin.read())[0])" 2> /dev/null)
-    if [[ "$VERSION" != "" ]]; then
-        printf $VERSION | $PYTHON -c "import os, sys; text = open(os.path.expanduser('/tmp/.bashrc'), 'r').read();open(os.path.expanduser('/tmp/.bashrc'), 'w').write(text.replace('REPLACE_WITH_VERSION', sys.stdin.read().strip()))"
-    fi
-
-    printf $COMMIT_HASH | $PYTHON -c "import os, sys; text = open(os.path.expanduser('/tmp/.bashrc'), 'r').read();open(os.path.expanduser('/tmp/.bashrc'), 'w').write(text.replace('REPLACE_WITH_REPO_HASH', sys.stdin.read().strip()))"
+# Without sed, we can't do find/replace.. so just use master
+if command -v sed &>/dev/null; then
+    HAS_SED=true
 else
-    # if we can't talk to the github api (maybe we're rate-limited?), just fall back to grabbing master.
+    HAS_SED=false
+fi
+
+# need sed and jq to use the hash
+if [[ $HAS_SED == "true" ]] && [[ $HAS_JQ == "true" ]]; then
+    USE_HASH=true
+else
+    USE_HASH=false
+fi
+
+CURL_LOW_TIMEOUT="curl -s --connect-timeout 1 --max-time 1 "
+
+if [[ $USE_HASH == true ]]; then
+
+    COMMIT_HASH=$($CURL_LOW_TIMEOUT https://api.github.com/repos/csm10495/dotfiles/branches/master | jq -r .commit.sha)
+
+    if [ -z "$COMMIT_HASH" ]; then
+        echo "Couldn't calculate commit hash.. falling back to master"
+        USE_HASH=false
+    else
+        # download corresponding files
+        $CURL_LOW_TIMEOUT https://raw.githubusercontent.com/csm10495/dotfiles/$COMMIT_HASH/home/.bash_profile > /tmp/.bash_profile
+        $CURL_LOW_TIMEOUT https://raw.githubusercontent.com/csm10495/dotfiles/$COMMIT_HASH/home/.bashrc > /tmp/.bashrc
+
+        # get the auto-incremented version number
+        VERSION=$($CURL_LOW_TIMEOUT -I "https://api.github.com/repos/csm10495/dotfiles/commits?per_page=1&sha=$COMMIT_HASH" | sed -n 's/.*&page=\(.*\)>.*/\1/p')
+
+        if [ -n "$VERSION" ]; then
+            # replace the version number in the bashrc
+            sed -i "s/REPLACE_WITH_VERSION/$VERSION/g" /tmp/.bashrc
+            sed -i "s/REPLACE_WITH_REPO_HASH/$COMMIT_HASH/g" /tmp/.bashrc
+        else
+            echo "Couldn't calculate version... falling back to master"
+            USE_HASH=false
+        fi
+    fi
+fi
+
+# Not using else since we could set USE_HASH to false in the above
+if [[ $USE_HASH == false ]]; then
     echo "Falling to legacy install behavior (either no commit hash or no python)"
-    $CURL_LOW_TIMEOUT -s https://raw.githubusercontent.com/csm10495/dotfiles/master/home/.bash_profile > /tmp/.bash_profile
-    $CURL_LOW_TIMEOUT -s https://raw.githubusercontent.com/csm10495/dotfiles/master/home/.bashrc > /tmp/.bashrc
+    $CURL_LOW_TIMEOUT https://raw.githubusercontent.com/csm10495/dotfiles/master/home/.bash_profile > /tmp/.bash_profile
+    $CURL_LOW_TIMEOUT https://raw.githubusercontent.com/csm10495/dotfiles/master/home/.bashrc > /tmp/.bashrc
 fi
 
 # Lets also add git-completion
